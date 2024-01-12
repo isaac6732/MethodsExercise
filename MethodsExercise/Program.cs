6392961c3de71af57ab3fe844d16300c1b98dc78
Isaac Oquendo <ioalvarez378@gmail.com>
2024-01-12 12:36:07 -0500

    Firs Portion

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //                                                               EXS 1
+
+            //Name: Isaac
+
+            //Favorite Color: Red
+
+            //Favorite Animal: Cat's
+
+            //Favorite Band: Bring Me The Horizon
+
+            Console.WriteLine("Hello - What is your firs name?");
+            var userName = Console.ReadLine();
+            
+            Console.WriteLine($"Hi, {userName} - What is your favorite color?");
+            var color = Console.ReadLine();
+            
+            Console.WriteLine($"{color} that is a nices color - What is your favorite animal?");
+            var animal = Console.ReadLine();
+           
+            Console.WriteLine("Nices! - What is your favorite band?");
+            var band = Console.ReadLine();
         }
     }
 }
